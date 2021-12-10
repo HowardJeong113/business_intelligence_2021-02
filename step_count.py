c7730415bd06d0a3084ca84fa19c48ba8a59b89a
HowardJeong113 <90450849+HowardJeong113@users.noreply.github.com>
2021-12-11 00:34:52 +0900

    Almost final commit

--- step_count.py
+++ step_count.py
@@ -1,6 +1,7 @@
 import csv
 from collections import defaultdict
 from datetime import datetime
+from pprint import pprint
 import math
 
 file_path = "com.samsung.shealth.step_daily_trend.202111072051.csv"
@@ -42,12 +43,11 @@ with open(file_path, newline='') as csvfile:
             date_to_count[date]['calorie'] += calorie
 
 
+# print("row : ", header)
+# print(data_list[0])
+# print("data length :", len(data_list))
 
-print("row : ", header)
-print(data_list[0])
-print("data length :", len(data_list))
-
-print(date_to_count)
+# print(date_to_count)
 
 # 요일별 평균 걸음걸이 출력
 
@@ -57,13 +57,12 @@ for k, v in date_to_count.items():
     weekday = v['weekday']
     weekday_count[weekday].append(v['count'])
 
-
 for k in sorted(weekday_count):
     v = weekday_count[k]
-    print(k, sum(v) / len(v))
+    # print(k, sum(v) / len(v))
 
 
-# 기온별 걸음걸이 
+# 기온별 걸음걸이
 
 weather_path = "weather.csv"
 with open(weather_path, newline='') as csvfile:
@@ -91,18 +90,76 @@ with open(weather_path, newline='') as csvfile:
 
 weather_count = defaultdict(list)
 
-
-
 for k, v in date_to_count.items():
-    date_weather = math.floor(date_to_weather[str(v['date'])])  
-    weather_grid = date_weather - date_weather % 3 
-    if weather_grid == 0:
-        print(v)
+    date_weather = math.floor(date_to_weather[str(v['date'])])
+    weather_grid = date_weather - date_weather % 3
+
     weather_count[weather_grid].append(v['count'])
 
 for k in sorted(weather_count.keys()):
     v = weather_count[k]
-    
-    if(len(v) > 0):
-        print(k, sum(v) / len(v))
 
+    # if(len(v) > 0):
+    #     print(k, sum(v) / len(v))
+
+weekday_weather_count = defaultdict(lambda: defaultdict(list))
+weekday_weather_avg = defaultdict(lambda: defaultdict(int))
+
+# 기온, 걸음걸이 합친 데이터
+for k, v in date_to_count.items():
+    weekday = v['weekday']
+
+    date_weather = math.floor(date_to_weather[str(v['date'])])
+    weather_grid = date_weather - date_weather % 3
+
+    weekday_weather_count[weekday][weather_grid].append(v['count'])
+
+for k in weekday_weather_count.keys():
+    for j in weekday_weather_count[k].keys():
+        v = weekday_weather_count[k][j]
+        if(len(v) > 0):
+            weekday_weather_avg[k][j] = sum(v) / len(v)
+
+
+pprint(weekday_weather_avg)
+print("초기 데이터 로드 완료")
+
+# (date_str, count, expected)
+user_input_data = []
+
+
+while True:
+    # 예상 기대치 조절
+    expected_mul_list = [1]
+    for date_str, count, expected in user_input_data[:10]:
+        expected_mul_list.append(count / expected)
+
+    expected_mul = sum(expected_mul_list) / len (expected_mul_list)
+
+    user_date = input("걸음수를 기록할 날짜를 입력해주세요. (YYYY-MM-DD 포맷으로 입력해주세요)")
+    user_date_weekday = datetime.fromisoformat(user_date).weekday()
+    if(user_date in date_to_weather):
+        user_date_weather = date_to_weather[user_date]
+    else:
+        new_date = user_date
+        while True:
+            new_date = str(int(new_date[0:4]) - 1) + new_date[4:]
+
+            if(new_date in date_to_weather):
+                user_date_weather = date_to_weather[new_date]
+                break
+            else:
+                continue
+
+    user_date_weather = math.floor(user_date_weather)
+    user_date_weather_grid = user_date_weather - user_date_weather % 3
+
+    user_step_count = int(input("위 날짜에 걸었던 걸음 수를 입력해주세요."))
+
+    expected = weekday_weather_avg[user_date_weekday][user_date_weather_grid] 
+
+    real_expected = expected_mul * expected
+
+    user_input_data.append((user_date, user_step_count, expected))
+
+    print("기대치는 ", real_expected, "입니다. (보정을 적용하지 않은 기대치는 ", expected, "입니다.) 달성률 : ", user_step_count / real_expected)
